83859e669209352de6b0fd3fc8a7f4665d0990b9
Shota Nozaki <emonkak@gmail.com>
2016-11-17 09:56:47 +0900

    Add methods to EnumerableInterface

--- src/EnumerableInterface.php
+++ src/EnumerableInterface.php
@@ -6,6 +6,12 @@ interface EnumerableInterface extends \Traversable
 {
     public function aggregate($seed, callable $func);
 
+    public function concat($second);
+
+    public function first(callable $predicate = null);
+
+    public function last(callable $predicate = null);
+
     public function join($inner, callable $outerKeySelector, callable $innerKeySelector, callable $resultSelector);
 
     public function groupJoin($inner, callable $outerKeySelector, callable $innerKeySelector, callable $resultSelector);

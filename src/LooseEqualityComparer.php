f749bf099e18db476feb4695378ed83f723c9984
Shota Nozaki <emonkak@gmail.com>
2020-02-19 02:04:35 +0900

    Fix a typing of `LooseEqualityComparer`

--- src/LooseEqualityComparer.php
+++ src/LooseEqualityComparer.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Emonkak\Enumerable;
 
 /**
- * @template T of ?scalar
  * @implements EqualityComparerInterface<?scalar>
  */
 class LooseEqualityComparer implements EqualityComparerInterface
